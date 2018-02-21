f3ed203f2b02e4b1d31770d11878c958141ed971
Simeon Petkov Stoykov <simeon_stoykov@mail.bg>
2018-02-21 21:54:06 +0200

    implement create operation

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function reverseStr(str) {
     return str.split('').reverse().join('');
 }
 
-let eventsData = [];
+let events = [];
 let previousData = '';
 
 let tcpClient = new net.Socket();
@@ -28,29 +28,68 @@ tcpClient.on('data', buffer => {
 
     for (let i = 0; i < dataArr.length; i++) {
         let currentData = dataArr[i];
-        console.log(currentData);
+        // console.log(currentData);
         let dataElements = reverseStr(currentData).split(/\|(?!\\)/g).filter(rec => rec !== '').reverse().map(rec => reverseStr(rec).replace(/\\\|/g, '|'));
         // console.log(dataElements);
-        let msgId = dataElements[0];
+        let msgId = Number(dataElements[0]);
         let operation = dataElements[1];
         let type = dataElements[2];
-        let timestamp = dataElements[3];
+        let timestamp = Number(dataElements[3]);
 
-        if (operation === 'create' && type === 'event') {
-            eventsData.push({
-                eventId: dataElements[4],
-                category: dataElements[5],
-                subCategory: dataElements[6],
-                name: dataElements[7],
-                startTime: dataElements[8],
-                display: dataElements[9],
-                suspended: dataElements[10],
-                markets: []
-            });
-
-            console.log(eventsData);
+        if (operation === 'create') {
+            switch (type) {
+                case 'event': {
+                    events.push({
+                        eventId: dataElements[4],
+                        category: dataElements[5],
+                        subCategory: dataElements[6],
+                        name: dataElements[7],
+                        startTime: Number(dataElements[8]),
+                        displayed: dataElements[9] === '1',
+                        suspended: dataElements[10] === '1',
+                        markets: []
+                    });
+                }
+                break;
+                case 'market': {
+                    let event = events.find(rec => rec.eventId === dataElements[4]);
+                    if (event) {
+                        event.markets.push({
+                            eventId: dataElements[4],
+                            marketId: dataElements[5],
+                            name: dataElements[6],
+                            displayed: dataElements[7] === '1',
+                            suspended: dataElements[8] === '1',
+                            outcomes: []
+                        });
+                    }
+                }
+                break;
+                case 'outcome': {
+                    for (let i = 0; i < events.length; i++) {
+                        let currentEvent = events[i];
+                        let market = currentEvent.markets.find(rec => rec.marketId === dataElements[4]);
+                        if (market) {
+                            market.outcomes.push({
+                                marketId: dataElements[4],
+                                outcomeId: dataElements[5],
+                                name: dataElements[6],
+                                price: dataElements[7],
+                                displayed: dataElements[8] === '1',
+                                suspended: dataElements[9] === '1'
+                            });
+                            break;
+                        }
+                    }
+                }
+                break;
+            }
+        } else if (operation === 'update') {
+            
         }
     }
+
+    console.log(events);
 
     // tcpClient.destroy();
 });
